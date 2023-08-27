ec9c8438d709aa92a80e85291c96a001bb724e80
hugocxl <corta.hugo@gmail.com>
2023-08-27 19:59:17 +0200

    Lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '{packages}/**/*': [
+  'packages/**/*': [
     'pnpm --reporter=silent lint:fix',
     'pnpm --reporter=silent format:fix'
   ],
-  '{packages}/**/*.css': ['pnpm --reporter=silent styles:fix'],
-  '{packages}/**/*.{ts,tsx}': "bash -c 'pnpm types:check'"
+  'packages/**/*.css': ['pnpm --reporter=silent styles:fix'],
+  'packages/**/*.{ts,tsx}': "bash -c 'pnpm types:check'"
 }
