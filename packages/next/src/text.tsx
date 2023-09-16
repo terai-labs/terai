1aa7862d20e0a99415bb6de8ebfe6c0796936582
hugocxl <corta.hugo@gmail.com>
2023-09-16 17:50:55 +0200

    Fix

--- packages/next/src/text.tsx
+++ packages/next/src/text.tsx
@@ -1,5 +1,3 @@
-'use server'
-
 // Dependencies
 import { createReactInterpolate } from '@rewordlabs/react'
 
--- playground/next-app/src/app/server.ts
+++ playground/next-app/src/app/server.ts
@@ -2,5 +2,5 @@ import { setupServer } from '@rewordlabs/next'
 
 export const { tx } = setupServer({
   loader: (locale: string, id: string) =>
-    import(`./${locale}/${id}.json`).then(msg => msg[id])
+    import(`../locale/${locale}/${id}.json`).then(msg => msg[id])
 })
