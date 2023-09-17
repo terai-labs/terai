8177c3e4e0239b56b1fb2eec7c8d81bf9e924ac4
hugocxl <corta.hugo@gmail.com>
2023-09-17 22:41:52 +0200

    fix: lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  'packages/**/*': [
+  '{packages,core}/**/*': [
     'pnpm --reporter=silent lint:fix',
     'pnpm --reporter=silent format:fix'
   ],
-  'packages/**/*.css': ['pnpm --reporter=silent styles:fix'],
-  'packages/**/*.{ts,tsx}': "bash -c 'pnpm types:check'"
+  '{packages,core}/**/*.css': ['pnpm --reporter=silent styles:fix'],
+  '{packages,core}/**/*.{ts,tsx}': "bash -c 'pnpm types:check'"
 }
