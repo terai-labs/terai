70d6f979ce6f017da5e90ef40f62dd4e07c74ca3
hugocxl <corta.hugo@gmail.com>
2023-09-18 07:55:24 +0200

    WIP

--- core/logger/tsup.config.ts
+++ core/logger/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  minify: true,
+  minify: false,
   shims: true,
   sourcemap: true,
   splitting: true
