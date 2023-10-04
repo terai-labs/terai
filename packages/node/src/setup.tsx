7ca67d7a7d317a268e918686b3ce8a29990cfc9e
Hugo Corta <corta.hugo@gmail.com>
2023-10-04 09:31:43 +0200

    feat: node package refact

--- packages/node/src/setup.tsx
+++ packages/node/src/setup.tsx
@@ -1,27 +1,55 @@
-// @ts-nocheck
 // Dependencies
-import { createTx } from '@rewordlabs/tx'
+import { createTx, type TxRenderProps } from '@rewordlabs/tx'
+import { observable } from '@legendapp/state'
+import { createFormat, interpolate } from '@rewordlabs/formatter'
 
 // Types
-import type { Locale } from '@rewordlabs/types'
+import type { Dictionaries, Loader, Locale } from '@rewordlabs/types'
 import type { InterpolateOptions } from '@rewordlabs/formatter'
-
-// Components
-import { text } from './text'
 
 export type SetupOptions = InterpolateOptions & {
   getLocale: () => Locale
-  loader: (locale: string, id: string) => Promise<string>
+  locales: Locale[]
+  loader: Loader
 }
 
-export const setup = ({ getLocale, loader, ...global }: SetupOptions) => {
-  const tx = createTx<Promise<string>, unknown>({
-    loader,
-    getLocale,
-    render: props => text({ ...props, global })
+type TxNodeRenderProps = TxRenderProps & { locale: Locale }
+
+export function setup({
+  loader,
+  locales,
+  getLocale,
+  format = {}
+}: SetupOptions) {
+  const state$ = observable<Dictionaries>({})
+  const getFormat = createFormat(getLocale)
+  const tx = createTx<string, TxNodeRenderProps>({
+    render: ({ id, variables, rawMessage, ...props }) => {
+      const locale = props.locale ?? getLocale()
+      const message = state$?.[locale]?.[id]?.get() ?? rawMessage
+      return interpolate(
+        {
+          message,
+          locale,
+          variables
+        },
+        {
+          format: {
+            ...format,
+            ...props.format
+          }
+        }
+      )
+    }
+  })
+
+  locales.map(async locale => {
+    const dic = (await loader(locale, locale)).default
+    state$[locale].set(dic)
   })
 
   return {
-    tx
+    tx,
+    getFormat
   }
 }
