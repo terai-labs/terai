8c2ced8677630c7d464d37f5a8de89f6f1106840
Hugo Corta <corta.hugo@gmail.com>
2023-10-20 20:23:04 +0200

    feat: review tsup config

--- packages/next/tsup.config.ts
+++ packages/next/tsup.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src'],
+  entry: [
+    'src/index.ts',
+    'src/client/index.ts',
+    'src/server/index.ts',
+    'src/link/index.ts',
+    'src/middleware/index.ts'
+  ],
   format: ['esm', 'cjs'],
   minify: true,
   shims: true,
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src'],
+  entry: [
+    'src/index.ts',
+    'src/core/client/index.ts',
+    'src/core/server/index.ts'
+  ],
   format: ['esm', 'cjs'],
   minify: true,
   shims: true,
