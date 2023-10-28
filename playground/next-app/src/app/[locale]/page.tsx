9a2cd64dd81086464fef480a1cc7127068be9032
Hugo Corta <corta.hugo@gmail.com>
2023-10-28 11:30:49 +0200

    feat: renamed dependencies version definition

--- playground/next-app/src/app/[locale]/page.tsx
+++ playground/next-app/src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
-import { Link } from '@koi18n/next/client'
-// const { Link } = require('@koi18n/next/client')
+// import { Link } from '@koi18n/next/client'
+const { Link } = require('@koi18n/next/client')
 
 export default async function Home() {
   return (
